34c4d238c0864ad368954ff72938018b9aab60f8
Kiên Nguyễn <108124347+KienNguyen1306@users.noreply.github.com>
2024-02-26 17:57:35 +0700

    dtaat

--- app/db/db.js
+++ app/db/db.js
@@ -4,16 +4,15 @@ let connection;
 
 try {
   connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASSWORD,
     port:3306,
-    // port:3001
-    // host: process.env.DB_HOST",
-    // user: "freedb_bosschua",
-    // database: "freedb_ndkshare",
-    // password: "qkgBV52ZtnDz!UM",
+    host: "sql.freedb.tech",
+    user: "freedb_bosschua",
+    database: "freedb_ndkshare",
+    password: "qkgBV52ZtnDz!UM",
   });
   console.log("Connected to MySQL database!");
 } catch (error) {
