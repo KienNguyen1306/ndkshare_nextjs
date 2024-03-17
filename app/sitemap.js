a4fda958b64ed64e3a8587a7a9c454ab104930e7
Kiên Nguyễn <108124347+KienNguyen1306@users.noreply.github.com>
2024-03-17 15:31:23 +0700

    sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,48 +1,28 @@
+import { fetchCoursesData, fetchGamesData } from "@/lib/fetch";
+
 export default async function sitemap() {
-  try {
-    const responseGames = await fetch(
-      `${process.env.NEXT_BASE_URL}/api/modgames?page=1&limit=99`
-    );
-    if (!responseGames.ok) {
-      console.error('Error fetching data:', responseCourses);
+  const gamesData = await fetchGamesData();
+  const games = gamesData.data.map((item) => ({
+    url: `${process.env.NEXT_BASE_URL}/modgame/${item.id}`,
+    lastModified: new Date(),
+    changeFrequency: "yearly",
+    priority: 1,
+  }));
 
-    }
-    const gamesData = await responseGames.json();
-    const games = gamesData.data.map((item) => ({
-      url: `${process.env.NEXT_BASE_URL}/modgame/${item.id}`,
-      lastModified: new Date(),
-      // changeFrequency: "yearly",
-      // priority: 1,
-    }));
+  const coursesData = await fetchCoursesData();
+  const courses = coursesData.data.map((item) => ({
+    url: `${process.env.NEXT_BASE_URL}/shareCourse/${item.id}`,
+    lastModified: new Date(),
+    changeFrequency: "yearly",
+    priority: 1,
+  }));
 
-    const responseCourses = await fetch(
-      `${process.env.NEXT_BASE_URL}/api/courses?page=1&limit=99`
-    );
-    if (!responseCourses.ok) {
-    console.error('Error fetching data:', responseCourses);
-    }
-    const coursesData = await responseCourses.json();
-    const courses = coursesData.data.map((item) => ({
-      url: `${process.env.NEXT_BASE_URL}/shareCourse/${item.id}`,
-      lastModified: new Date(),
-      // changeFrequency: "yearly",
-      // priority: 1,
-    }));
+  const routes = ["", "/sharecourse", "/contact", "/login"].map((route) => ({
+    url: `${process.env.NEXT_BASE_URL}${route}`,
+    lastModified: new Date(),
+    changeFrequency: "yearly",
+    priority: 1,
+  }));
 
-    const routes = ["", "/sharecourse", "/contact", "/login"].map((route) => ({
-      url: `${process.env.NEXT_BASE_URL}${route}`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    }));
-
-    return [
-      ...routes,
-      ...games,
-      ...courses,
-    ];
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
+  return [...routes, ...games, ...courses];
 }
